60562f0d0312797e7027999feff08fd69a3f01e5
jinyang <k.you@ingage.jp>
2023-08-22 16:30:29 +0900

    aboutを追加する

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get static_pages_home_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /help" do
     it "returns http success" do
-      get "/static_pages/help"
+      get static_pages_help_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /about" do
+    it "returns http success" do
+      get static_pages_about_path
       expect(response).to have_http_status(:success)
     end
   end
