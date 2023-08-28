5c4f2be7c3651526051ee4ba9de101a8739c4047
jinyang <k.you@ingage.jp>
2023-08-28 10:26:29 +0900

    各ページのタイトル変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe "GET /home" do
     it "returns http success" do
       get static_pages_home_path
@@ -25,21 +27,21 @@ RSpec.describe "StaticPages", type: :request do
   describe "title /home" do
     it "Homeがタイトルに含まれること" do
       get static_pages_home_path
-      expect(response.body).to include "<title>Home</title>"
+      expect(response.body).to include "<title>Home | #{base_title}</title>"
     end
   end
 
   describe "title /help" do
     it "Helpがタイトルに含まれること" do
-      get static_pages_home_path
-      expect(response.body).to include "<title>Help</title>"
+      get static_pages_help_path
+      expect(response.body).to include "<title>Help | #{base_title}</title>"
     end
   end
 
   describe "title /About" do
     it "Aboutがタイトルに含まれること" do
-      get static_pages_home_path
-      expect(response.body).to include "<title>About</title>"
+      get static_pages_about_path
+      expect(response.body).to include "<title>About | #{base_title}</title>"
     end
   end
 end
