5f56fc4a83e9e12892669e16347dc6121e249992
k.you <k.you@ingage.jp>
2023-09-13 16:15:49 +0900

    リンクページテスト

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -65,4 +65,24 @@ RSpec.describe 'StaticPages', type: :request do
       expect(response.body).to include "<title>Contact | #{base_title}</title>"
     end
   end
+
 end
+
+RSpec.describe "StaticPages", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'root' do
+    it 'root_pathへのリンクが二つ、help、about、contactへのリンクが表示されていること' do
+      visit root_path
+
+      expect(page).to have_link 'sample app', href: root_path
+      expect(page).to have_link 'Home', href: home_path
+      expect(page).to have_link 'Help', href: help_path
+      expect(page).to have_link 'Contact', href: contact_path
+      expect(page).to have_link 'About', href: about_path
+    end
+  end
+end
+
