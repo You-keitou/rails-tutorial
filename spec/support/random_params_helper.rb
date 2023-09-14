59ba4843a207217708901cc549edc8ad7af14982
k.you <k.you@ingage.jp>
2023-09-14 17:04:59 +0900

    ランダムユーザーパラメーターヘルパー

--- spec/support/random_params_helper.rb
+++ spec/support/random_params_helper.rb
@@ -1,5 +1,12 @@
 module RandomParamsHelper
 	def random_alphbet_sequence(len)
-		Array.new(51){rand(26)}.map{|n| ('a'..'z').to_a[n] }.join()
+		Array.new(len){rand(26)}.map{|n| ('a'..'z').to_a[n] }.join()
+	end
+
+	def random_user_params
+		{
+			name: Faker::Name.last_name,
+			email: Faker::Internet.free_email
+		}
 	end
 end
