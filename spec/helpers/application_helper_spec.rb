ff2bde96a5d5b323d627e714b9d45f5159ee9aa2
k.you <k.you@ingage.jp>
2023-09-14 17:04:13 +0900

    full_titleヘルパーテスト修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 # 引数ありの場合、　引数なしの場合
 RSpec.describe 'full_title_helper', type: :helper do
-  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
-  describe 'full_title one argument' do
-    it 'full_titleヘルパーに引数が与えられた時に、きちんと表示すること' do
-      argument = 'About us'
-      expect(full_title('About us')).to eq "#{argument} | #{base_title}"
+  describe 'full_title' do
+    let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+    context '引数あり' do
+      it '引数とベースタイトルが合わせて表示されること' do
+        argument = 'About us'
+        expect(full_title('About us')).to eq "#{argument} | #{base_title}"
+      end
     end
-  end
-
-  describe 'full_title no argument' do
-    it 'full_titleヘルパーに引数が与えられない時に、base_titleのみかえること' do
-      expect(full_title).to eq base_title
+    context '引数なし' do
+      it 'ベースタイトルのみ表示されること' do
+        expect(full_title).to eq base_title
+      end
     end
   end
 end
