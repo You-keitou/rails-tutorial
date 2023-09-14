d9ec369f916e95ea49190ce9023193db7a22b8d8
k.you <k.you@ingage.jp>
2023-09-14 16:47:00 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }
 end
