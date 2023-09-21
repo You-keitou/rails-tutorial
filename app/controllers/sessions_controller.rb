6d6ef53ddbadb740d4fd020aa072c5a7705382fc
k.you <k.you@ingage.jp>
2023-09-21 11:36:02 +0900

    Session helperの導入

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email].downcase)
     if user && user.authenticate(user_params[:password])
-
+      log_in(user)
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
