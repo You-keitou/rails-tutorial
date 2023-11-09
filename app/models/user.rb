6e9cff612931c17d05368e87d1832af0dfdc6c84
You-Keitou <youkeitou327@gmail.com>
2023-11-10 07:00:18 +0900

    新しいrelationshipテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
 
   has_secure_password
 
