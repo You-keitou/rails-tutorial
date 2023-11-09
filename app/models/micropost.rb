344f2dba5a458989bac2e2afcbdd82bb88b6e178
You-Keitou <youkeitou327@gmail.com>
2023-11-10 03:54:21 +0900

    デフォルトスコープ　

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,6 +19,7 @@
 #
 class Micropost < ApplicationRecord
   belongs_to :user
+  default_scope -> { order( created_at: :desc )}
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
